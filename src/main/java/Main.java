aadd871ccf23109813209e60d11923122c15a2c9
ns1992 <nick.scannell@hotmail.co.uk>
2021-01-06 16:08:29 +0000

    Merge Sort Implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // more comments to generate diffs
     }
 }
--- src/main/java/searching/MergeSort.java
+++ src/main/java/searching/MergeSort.java
@@ -3,6 +3,43 @@ package searching;
 public class MergeSort {
 
     public void sort(int[] array) {
-            // Comment made on main to test Merging
+        int[] helper = new int[array.length];
+        sort(array, helper, 0, array.length - 1);
+    }
+
+    private void sort(int[] array, int[] helper, int low, int high) {
+        if (low < high) {
+            int middle = low + (high - low) / 2;
+            sort(array, helper, low, middle);
+            sort(array, helper, middle + 1, high);
+            merge(array, helper, low, middle, high);
+        }
+    }
+
+
+    private void merge(int[] array, int[] helper, int low, int middle, int high) {
+        for (int i = low; i <= high; i++) {
+            helper[i] = array[i];
+        }
+
+        int helperLeft = low;
+        int helperRight = middle + 1;
+        int current = low;
+
+        while (helperLeft <= middle && helperRight <= high) {
+            if (helper[helperLeft] <= helper[helperRight]) {
+                array[current] = helper[helperLeft];
+                helperLeft++;
+            } else {
+                array[current] = helper[helperRight];
+                helperRight++;
+            }
+            current++;
+        }
+
+        int remaining = middle - helperLeft;
+        for (int i = 0; i <= remaining; i++) {
+            array[current + i] = helper[helperLeft + i];
+        }
     }
 }
